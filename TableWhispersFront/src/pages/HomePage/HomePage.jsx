de022c8799cb546dbfdd1db4a2df73d2d99a39a9
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2025-05-05 16:00:05 +0300

    check

--- TableWhispersFront/src/pages/HomePage/HomePage.jsx
+++ TableWhispersFront/src/pages/HomePage/HomePage.jsx
@@ -192,7 +192,7 @@ const HomePage = () => {
   const fetchRecommendedRestaurants = async () => {
     try {
       //const response = await fetch('http://localhost:5000/all_Restaurants_Data');  // https://tablewhispersbackend-ggfxhehwg2egdaa7.westeurope-01.azurewebsites.net
-      const response = await fetch('https://tablewhispersbackend-ggfxhehwg2egdaa7.westeurope-01.azurewebsites.net/all_Restaurants_Data',{
+      const response = await fetch('tablewhispersbackend.azurewebsites.net/all_Restaurants_Data',{
         mode: 'no-cors' 
       });
       const data = await response.json();
